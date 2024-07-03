82059bd020684afe9fa6b1b07c717877602e072c
luke <ivan1.krasikov.2003@gmail.com>
2024-07-03 14:39:25 +0300

    realize models and fixtures

---
+++ src/DataFixtures/CourseFixtures.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Course;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class CourseFixture extends Fixture
+{
+    private array $data = [
+        [
+            'chars_code' => 'english-language',
+            'title' => 'Английский язык',
+            'description' => 'Обучение основной грамматике английского языка',
+        ],
+        [
+            'chars_code' => 'math',
+            'title' => 'Математика',
+            'description' => 'Обучение алгебре и геометрии, весь школьный курс',
+        ],
+        [
+            'chars_code' => 'chinesse-language',
+            'title' =>  'Китайский язык',
+            'description' => 'Обучение китайского языка и культуры Китая',
+        ],
+        [
+            'chars_code' => 'history-of-russia',
+            'title' => 'История России',
+            'description' => 'История России с древних времён до нашей эры, простым языком',
+        ],
+        [
+            'chars_code' => 'physics',
+            'title' => 'Физика',
+            'description' => 'Обучение физики, основных понятий и теории, простым языком',
+        ],
+    ];
+
+    public function load(ObjectManager $manager): void
+    {
+        
+        foreach ($this->data as $courseData) {
+            $course = new Course();
+            $course->setCharsCode($courseData['chars_code'])
+                ->setTitle($courseData['title'])
+                ->setDescription($courseData['description']);
+            $manager->persist($course);
+        }
+        $manager->flush();
+    }
+}
---
+++ src/Entity/Lesson.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\LessonRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\DBAL\Types\Types;
+
+#[ORM\Entity(repositoryClass: LessonRepository::class)]
+class Lesson
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\ManyToOne(inversedBy: 'lessons')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Course $course = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $title = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $content = null;
+
+    #[ORM\Column(options: ['check' => 'ordering <= 10000'])]
+    private ?int $ordering = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCourse(): ?Course
+    {
+        return $this->course;
+    }
+
+    public function setCourse(?Course $course): static
+    {
+        $this->course = $course;
+
+        return $this;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): static
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): static
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function getOrdering(): ?int
+    {
+        return $this->ordering;
+    }
+
+    public function setOrdering(int $ordering): static
+    {
+        $this->ordering = $ordering;
+
+        return $this;
+    }
+}
